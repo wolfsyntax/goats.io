58e843d6af4ac90f229589db87ca8e1ba6a9645a
Wolf Syntax <wolfsyntax@yahoo.com>
2018-11-13 23:01:53 +0800

    Adding DOM PDF

---
+++ application/libraries/pdf.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+define('DOMPDF_ENABLE_AUTOLOAD', false);
+require_once("./vendor/dompdf/autoload.inc.php");
+
+class Pdf {
+
+  public function generate($html, $filename='', $stream=TRUE, $paper = 'A4', $orientation = "portrait")
+  {
+
+    $dompdf = new DOMPDF();
+    $dompdf->load_html($html);
+    $dompdf->set_paper($paper, $orientation);
+    $dompdf->render();
+
+    if ($stream) {
+
+        $dompdf->stream($filename.".pdf", array("Attachment" => 1));
+
+    } else {
+
+        return $dompdf->output();
+
+    }
+
+  }
+}
