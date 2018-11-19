5add1597b080d2541b559b2080d3562c97d71b17
Paolo Enciso <penciso@gbox.adnu.edu.ph>
2018-11-20 04:26:48 +0800

    Additional UI for Goat Module (Add Goat and Goat Sales)

--- application/views/goats/add_goats.php
+++ application/views/goats/add_goats.php
@@ -1,55 +1,90 @@
-<div class="container-fluid">
 
-	<div class="row font-weight-bold">
-		<!-- Mobile and Desktop-->
-		<div class="col-6 col-md-4 py-1 bg-success text-sm-center">Apple</div>
-		<div class="col-6 col-md-4 py-1 bg-warning text-md-center">Ball</div>
-		<div class="col-12 col-md-4 py-1 bg-danger text-lg-center text-lg-left">Cat</div>
-	</div>
+<div class="col-md-8 offset-md-2">
+    <span class="anchor" id="formUserEdit"></span>
+                    <hr class="my-3">
 
-	<div class="row mt-md-1 font-weight-normal">
-		<div class="col-12 col-sm-12 col-lg-4 py-1 bg-primary text-left text-white" >1</div>
-		<div class="col-6 col-sm-6 col-lg-4 py-1 bg-secondary text-center text-white ">2</div>
-		<div class="col-6 col-sm-6 col-lg-4 py-1 bg-info text-right text-white text-truncate">the quick brown fox jumps over the lazy dog</div>
-		
-		<div class="col-12 col-sm-12 d-none d-sm-none d-md-block py-1 bg-dark text-center text-white mt-3 mt-md-0">4</div>
-		
-	</div>
-
-	<div class="row font-weight-light">
-		<div class="col-12">
-			<div class="container-fluid text-center font-weight-bold text-capitalize text-truncate">
-				<div class="row">
-					<div class="col-12 col-md-2">
-						<div class="alert alert-success" role="alert">
-							<button type="button" class="close" data-dismiss="alert" aria-label="Close">&times;</button>
-							<div class="">
-								<span></span><strong>Invalid</strong>
-								<span class="text-muted"><p class="font-weight-light">Username or Password</p></span>
-							</div>
-						</div>
-
-					</div>
-					<div class="clearfix"></div>
-				</div>
-				<div class="row font-italic">
-					<div class="col-12">
-						<div class="card-deck bg-info">
-							<div class="card bg-light py-1">A</div>
-							<div class="card bg-light py-1">B</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="col-12">
-						<a tabindex="1" class="btn btn-lg btn-danger" role="button" data-toggle="popover" data-trigger="focus" title="Dismissible popover" data-content="And here's some amazing content. It's very engaging. Right?" data-placement="left" id="err_username">Dismissible popover</a>
-						<a tabindex="0" class="btn btn-lg btn-danger" role="button" data-toggle="popover" data-trigger="focus" title="Dismissible popover" data-content="And here's some amazing content. It's very engaging. Wrong?">Dismissible popover</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                    <!-- form user info -->
+                    <div class="card card-outline-secondary">
+                        <div class="card-header">
+                            <h3 class="mb-0">Add Goat</h3>
+                        </div>
+                        <div class="card-body">
+                            <form class="form" role="form" autocomplete="off">
+                                <div class="form-group row">
+                                    <label class="col-lg-2 col-form-label form-control-label">EarTag ID:</label>
+                                    <div class="col-lg-9">
+                                        <input class="form-control" type="text" value="">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                     <label class="col-lg-2 col-form-label form-control-label">Tag Color:</label>
+                                    <div class="col-lg-9">
+                                        <input class="form-control" type="text" value="">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-lg-2 col-form-label form-control-label">Body Color:</label>
+                                    <div class="col-lg-9">
+                                        <input class="form-control" type="email" value="">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-lg-2 col-form-label form-control-label">Birthdate:</label>
+                                    <div class="col-lg-9">
+                                        <input class="form-control" type="date" value="yyyy-mm-dd" id="example-date-input">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-lg-2 col-form-label form-control-label">Website</label>
+                                    <div class="col-lg-9">
+                                        <input class="form-control" type="url" value="">
+                                    </div>
+                                </div>
 
 
-</div>
+
+                                <div class="form-group row">
+                                    <label class="col-lg-2 col-form-label form-control-label">Gender:</label>
+                                    <div class="col-lg-9">
+                                        <label class="form-check-label">
+                                        &nbsp; &nbsp; &nbsp;<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> Male
+                                    </label>
+                                    <div class="form-check form-check-inline">
+                                    <label class="form-check-label">
+                                       &nbsp;&nbsp;&nbsp; <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> Female
+                                    </label>
+                                </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="form-group row">
+                                    <label class="col-lg-2 col-form-label form-control-label">Sire Tag ID</label>
+                                    <div class="col-lg-9">
+                                        <input class="form-control" type="url" value="">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-lg-2 col-form-label form-control-label">Dam Tag ID</label>
+                                    <div class="col-lg-9">
+                                        <input class="form-control" type="url" value="">
+                                    </div>
+                                </div>
+                                
+                                <div class="form-group row">
+                                    <label class="col-lg-2 col-form-label form-control-label"></label>
+                                    <div class="col-lg-9">
+                                        <input type="reset" class="btn btn-secondary" value="Cancel">
+                                        <input type="button" class="btn btn-primary" value="Save Changes">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /form user info -->
+
+                </div>
+</body>
+</html> 
+
+
