92ae2ac8b0c8cb3d4cb3547d3565ddf6bfe9246e
Wolf Syntax <wolfsyntax@yahoo.com>
2018-11-22 03:00:14 +0800

    Remove unnecessary HTML Tags from forms (goats and transaction) [Continuation]

--- application/views/transactions/goat_sales.php
+++ application/views/transactions/goat_sales.php
@@ -1,11 +1,4 @@
-<html>
-	<head>
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-		<link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-		<script src="js/bootstrap.min.js"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	</head>
-<body>
+
 <div class="col-md-8 offset-md-2">
     <span class="anchor" id="formUserEdit"></span>
     <hr class="my-3">
@@ -13,18 +6,18 @@
     <!-- form user info -->
     <div class="card card-outline-secondary">
         <div class="card-header">
-            <h3 class="mb-0"><i class="fas fa-coins">&nbsp;</i>Goat Sales</h3>
+            <h3 class="mb-0">Goat Sales</h3>
         </div>
 
         <div class="card-body">
             
-        <form>
+            <?= form_open(); ?>
             
             <div class="form-group row">
                 
-                <label class="col-lg-2 col-form-label form-control-label" style="margin-left:130px;">Sale Date</label>
+                <label class="col-lg-2 col-form-label form-control-label">Sale Date:</label>
                 
-                <div class="col-lg-6">
+                <div class="col-lg-9">
                     <input class="form-control" type="date" value="yyyy-mm-dd" id="example-date-input">
                 </div>
 
@@ -33,11 +26,11 @@
 
             <div class="form-group row">
                 
-                <label class="col-lg-2 col-form-label form-control-label" style="margin-left:130px;">EarTag ID</label>
+                <label class="col-lg-2 col-form-label form-control-label">EarTag ID:</label>
 
-                <div class="col-lg-6">
+                <div class="col-lg-9">
 
-                    <input class="form-control" type="text" placeholder=" " value="">
+                    <input class="form-control" type="text" placeholder="######" value="">
 
                 </div>
 
@@ -45,9 +38,9 @@
                                 
             <div class="form-group row">
                 
-                <label class="col-lg-2 col-form-label form-control-label" style="margin-left:130px;">Buyer</label>
+                <label class="col-lg-2 col-form-label form-control-label">Buyer:</label>
                 
-                <div class="col-lg-6">
+                <div class="col-lg-9">
                 
                     <input class="form-control" type="text" value="">
                 
@@ -57,23 +50,21 @@
 
             <div class="form-group row">
                 
-                <label class="col-lg-2 col-form-label form-control-label" style="margin-left:130px;">Weight</label>
+                <label class="col-lg-2 col-form-label form-control-label">Weight:</label>
                 
-                <div class="col-lg-6">
-                    <div class="input-group">
-                        <input class="form-control" type="email" placeholder="" value="">
-                        <div class="input-group-append">
-                                <span class="input-group-text">kls</span>
-                        </div>
-                    </div>
+                <div class="col-lg-9">
+                
+                    <input class="form-control" type="email" placeholder="kls"value="">
+                
                 </div>
+
             </div>
                                
             <div class="form-group row">
                 
-                <label class="col-lg-2 col-form-label form-control-label" style="margin-left:130px;">Price</label>
+                <label class="col-lg-2 col-form-label form-control-label">Price:</label>
                 
-                <div class="col-lg-6">
+                <div class="col-lg-9">
                     
                     <div class="input-group">
                     
@@ -91,9 +82,9 @@
 
             <div class="form-group row">
                     
-                <label class="col-lg-2 col-form-label form-control-label" style="margin-left:130px;">Description:</label>
+                <label class="col-lg-2 col-form-label form-control-label">Description:</label>
                     
-                <div class="col-lg-6">
+                <div class="col-lg-9">
                     
                     <textarea class="form-control" id="exampleMessage"></textarea>
                     
@@ -103,17 +94,19 @@
 
             <div class="form-group row">
             
-                <label class="form-control-label" style="margin-left:130px;"></label>
+                <label class="col-lg-2 col-form-label form-control-label"></label>
                     
-                <div class="col-lg-6">
+                <div class="col-lg-9">
             
-                    <input type="button" style="margin-left:145px;" class="btn btn-primary" value="Add Sale">
+                    <input type="reset" class="btn btn-secondary" value="Cancel">
+            
+                    <input type="button" class="btn btn-primary" value="Save Changes">
             
                 </div>
             
             </div>
             
-            </form>
+            <?= form_close();?>
 
         </div>
 
@@ -121,6 +114,5 @@
 
 </div>
 
-</body>
-</html>
+
 
