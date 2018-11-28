5f3e9674cbe9c6cbf85de8fee05837540eb5aa63
Wolf Syntax <wolfsyntax@yahoo.com>
2018-11-28 10:40:54 +0800

    Modify Index page

--- application/views/includes/user_header.php
+++ application/views/includes/user_header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light col-12">
   <a class="navbar-brand text-center" href="#">&emsp;G.O.A.T.S</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <i class="navbar-toggler-icon"></i>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -10,31 +10,31 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li-->
       <li class="nav-item">
-        <a class="nav-link" href="<?= base_url() ?>dashboard" title="Dashboard"><span class="fa fa-user-circle"></span><span class="d-block-inline d-sm-block-inline d-md-block-inline d-lg-none ">&emsp;<?= $this->session->userdata('user_fname'); ?></span></a>
+        <a class="nav-link" href="<?= base_url() ?>dashboard" title="Dashboard"><i class="fa fa-user-circle"></i><span class="d-block-inline d-sm-block-inline d-md-block-inline d-lg-none ">&emsp;<?= $this->session->userdata('user_fname'); ?></span></a>
       </li>
 
-      <li class="nav-item">
+      <li class="nav-item" style="position: relative; float: left;">
         <a class="nav-link" href="<?= base_url() ?>dashboard" title="Dashboard">
-          <?php if($this->session->userdata('notif') == 0){ ?>
-            <span class="fa fa-bell-o">
+          <?php if($this->session->userdata('notif') == 1){ ?>
+            <i class="fa fa-bell-o badge">
           <?php } else { ?>
-            <span class="fa fa-bell">
+            <i class="fa fa-bell">
           <?php } ?>
-            </span><span class="d-block-inline d-sm-block-inline d-md-block-inline d-lg-none ">&emsp;Notifications</span>
+            </i><span class="d-block-inline d-sm-block-inline d-md-block-inline d-lg-none ">&emsp;Notifications</span>
         </a>
       </li>
 
       <li class="nav-item dropdown">
         <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <span class="fa fa-cog text-dark"></span><span class="d-block-inline d-sm-block-inline d-md-block-inline d-lg-none ">&emsp;Settings</span>
+          <i class="fa fa-cog text-dark"></i><span class="d-block-inline d-sm-block-inline d-md-block-inline d-lg-none ">&emsp;Settings</span>
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
           <p class="dropdown-item" href="">Signed in as<br/><strong class="text-dark"><?= $this->session->userdata('username'); ?></strong></p>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="<?= base_url() ?>profile/settings"><span class="fa fa-question-circle"></span>&emsp;Help</a>
-          <a class="dropdown-item" href="<?= base_url() ?>profile/settings"><span class="fa fa-user"></span>&emsp;Your Profile</a>
+          <a class="dropdown-item" href="<?= base_url() ?>profile/settings"><i class="fa fa-question-circle"></i>&emsp;Help</a>
+          <a class="dropdown-item" href="<?= base_url() ?>profile/settings"><i class="fa fa-user"></i>&emsp;Your Profile</a>
           
-          <a class="dropdown-item" href="<?= base_url() ?>logout"><span class="fa fa-sign-out"></span>&emsp;Sign out</a>
+          <a class="dropdown-item" href="<?= base_url() ?>logout"><i class="fa fa-sign-out"></i>&emsp;Sign out</a>
         </div>
       </li>
       
